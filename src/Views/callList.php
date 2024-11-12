036adcf669fe52a5c945fc9090b46b18336190b9
Mangel111111111 <mangel111111111@gmail.com>
2024-11-12 15:21:09 +0100

    style: add icon img and link layout.php to footer.php and header.php

---
+++ src/Views/callList.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("Components/layout.php")
+?>
+
+<body>
+<?php
+    require_once("Components/header.php")
+?>
+    <main>
+
+    </main>
+    <?php
+    require_once("Components/footer.php")
+?>
+</body>
