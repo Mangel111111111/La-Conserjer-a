f8035738a9c6a0d1585f1e19bcd7db6584302bcb
Kalixto73 <Kalixto75@gmail.com>
2024-11-13 12:05:58 +0100

    feat: FunctionDelete added to CallController

--- src/Controllers/CallController.php
+++ src/Controllers/CallController.php
@@ -8,6 +8,12 @@
     class CallController {
 
         public function __construct(){
+            
+            if(isset($_GET["action"]) && (isset($_GET["action"]) == "delete")){
+                $this->delete($_GET["id"]);
+                return;
+            }
+
             $this->index();
         }
 
@@ -15,6 +21,13 @@
            $call = new Call;
            $calls = $call->all();
            new View("callList", ["call" => $calls]);
+        }
+        
+        public function delete($id){
+            $callDelete = new Call;
+            $call = $callDelete->findById($id);
+            $call ->destroy();
+            $this ->index();
         }
     }
 
