2321a0e9916acad7e3be6084c26a3cc6e6df77d5
Mangel111111111 <mangel111111111@gmail.com>
2024-11-13 15:07:44 +0100

    feat: add footer

---
+++ src/Views/Components/footer.php
@@ -0,0 +1,24 @@
+<footer>
+  <div class="footerContent">
+    <div class="icons">
+      <a href="https://github.com/Mangel111111111" target="_blank">
+        <img src="./public/img/icons/githubLogo.png" alt="GitHub Icon" class="githubIcon">
+      </a>
+      <a href="https://github.com/angiehelensanchez" target="_blank">
+        <img src="./public/img/icons/githubLogo.png" alt="GitHub Icon" class="githubIcon">
+      </a>
+      <a href="https://github.com/Inma0711" target="_blank">
+        <img src="./public/img/icons/githubLogo.png" alt="GitHub Icon" class="githubIcon">
+      </a>
+      <a href="https://github.com/Kalixto73a" target="_blank">
+        <img src="./public/img/icons/githubLogo.png" alt="GitHub Icon" class="githubIcon">
+      </a>
+      <a href="https://github.com/ugotchriscoded" target="_blank">
+        <img src="./public/img/icons/githubLogo.png" alt="GitHub Icon" class="githubIcon">
+      </a>
+    </div>
+    <div class="footerText">
+        Copyright © 2024 ︱ Hotel Incident Reporting Website
+    </div>
+  </div>
+</footer>
