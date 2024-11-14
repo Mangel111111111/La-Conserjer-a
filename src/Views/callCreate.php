3230c8715e7eafb4fc25a4acc2b1f0c2576d5b3a
angiehelensanchez <angiehelensanchez@gmail.com>
2024-11-14 10:41:08 +0100

    style: remove initial style

--- src/Views/callCreate.php
+++ src/Views/callCreate.php
@@ -11,28 +11,28 @@
         <h2>Añadir nueva incidencia</h2>
 
         <a href="./index.php">
-            <button type="button" id="button-cancel" class="btn btn-outline-danger button-cancel">Cancelar</button>
+            <button type="button" id="button-cancel" >Cancelar</button>
         </a>
 
-        <form class="justify-content-center" action="?action=store" method="post">
-            <div class="input-group" style="margin-bottom: 2rem; width: 90%; margin-left: 5%">
-                <span class="input-group-text">Nº de Habitación</span>
+        <form action="?action=store" method="post">
+            <div>
+                <span>Nº de Habitación</span>
                 <input type="text" name="room" required placeholder="Nº de Habitación">
             </div>
 
-            <div class="input-group" style="margin-bottom: 2rem; width: 90%; margin-left: 5%">
-                <span class="input-group-text">Incidencia</span>
-                <textarea class="form-control" area-label="Width textarea" type="text" name="issue" required placeholder="Descripción de la incidencia"></textarea>
+            <div>
+                <span>Incidencia</span>
+                <textarea area-label="Width textarea" type="text" name="issue" required placeholder="Descripción de la incidencia"></textarea>
             </div>
 
-            <div class="input-group" style="margin-bottom: 2rem; width: 90%; margin-left: 5%">
-                <span class="input-group-text">Fecha</span>
+            <div>
+                <span>Fecha</span>
                 <input type="date" name="dateTime" required>
             </div>
 
-            <div class="botones" style="margin-bottom: 2rem;">
-                <input type="submit" value="Confirmar" class="btn-lg btn-outline-success go-add-task float">
-                <input type="reset" value="Borrar" class="btn-lg btn-outline-warning go-add-task float">
+            <div>
+                <input type="submit" value="Confirmar">
+                <input type="reset" value="Borrar">
             </div>
         </form>
     </main>
